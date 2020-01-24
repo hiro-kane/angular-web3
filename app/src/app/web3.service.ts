08d6fdf882760385a40fc076cc4a0080b2866014
h.kankeo <h.kaneko@pro-japan.co.jp>
2020-01-24 10:36:00 +0000

    web3連携アプリ存在確認

--- app/src/app/web3.service.ts
+++ app/src/app/web3.service.ts
@@ -10,7 +10,7 @@ import { SimpleStorage } from './contract/simpleStorage';
 })
 export class Web3Service {
 
-  loginAccount: string = "";
+  loginAccount: string = undefined;
   web3Subject: BehaviorSubject<Web3> = new BehaviorSubject<Web3>(undefined);
 
   constructor() {
@@ -20,12 +20,11 @@ export class Web3Service {
 
       // windowのethereumを取得
       const web3 = new Web3(window['ethereum']);
-
       // アカウント情報取得
       const accounts = await web3.eth.getAccounts();
 
-      // 取得できたアカウント数でWalletがUnlock済か判定
-      if (accounts.length === 0)
+      // アカウントが取得できたかどうかでロック中か判定
+      if (accounts[0] === undefined)
         return;
 
       // 取得できている場合はアカウント情報取得&監視処理を実行
@@ -36,12 +35,13 @@ export class Web3Service {
   /**
    * WalletUnlock
    */
-  async　unlockWallet() {
+  async unlockWallet() {
+    // metamaskアプリを登録していない場合は処理しない
+    if (!this.existsWeb3App()) return;
+
     // アンロック済の場合は処理しない
     if (this.isUnlockWallet()) return;
 
-    // metamaskアプリを登録していない場合は処理しない
-    if (!this.existsWeb3App()) return;
     try {
       // ログイン
       await window['ethereum'].enable();
@@ -55,7 +55,7 @@ export class Web3Service {
    * Walletアンロック済か判定
    */
   isUnlockWallet(): boolean {
-    return (this.loginAccount !== "");
+    return (this.loginAccount !== undefined);
   }
 
   /**
@@ -70,7 +70,6 @@ export class Web3Service {
    */
   getLoginAccount(): string {
     if (!this.isUnlockWallet()) return;
-
     return this.loginAccount;
   }
 
@@ -92,15 +91,12 @@ export class Web3Service {
 
       // アカウント情報の値変更を検知しWeb3情報を更新
       if (this.loginAccount !== accounts[0]) {
-        // undefinedかどうかでアカウント切り変え or ログアウトを判定
-        if (accounts[0] !== undefined) {
-          // アカウント切り替え
-          this.loginAccount = accounts[0];
-          this.web3Subject.next(web3);
-        } else {
+        this.loginAccount = accounts[0];
+        this.web3Subject.next(web3);
+
+        // ログアウトの場合は監視終了
+        if (accounts[0] === undefined) {
           // ログアウト
-          this.loginAccount = "";
-          this.web3Subject.next(web3);
           moniter.unsubscribe();
         }
       }
